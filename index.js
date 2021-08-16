47d5b66272b66c575e1f9929d46858d5eab1e751
tepe16 <39243063+tepe16@users.noreply.github.com>
2021-08-17 03:30:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ setInterval(() => {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/pwa-examples/a2hs/sw.js')
+    .register('/sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
 
