b403f4512e0f8e1b2c092d746d522d5a45cb0203
tepe16 <39243063+tepe16@users.noreply.github.com>
2021-08-17 03:36:28 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
-      '/pwa-examples/a2hs/',
-      '/pwa-examples/a2hs/index.html',
-      '/pwa-examples/a2hs/index.js',
-      '/pwa-examples/a2hs/style.css',
-      '/pwa-examples/a2hs/images/fox1.jpg',
-      '/pwa-examples/a2hs/images/fox2.jpg',
-      '/pwa-examples/a2hs/images/fox3.jpg',
-      '/pwa-examples/a2hs/images/fox4.jpg',
+      '/index.html',
+      '/index.js',
+      '/style.css',
+      '/images/fox1.jpg',
+      '/images/fox2.jpg',
+      '/images/fox3.jpg',
+      '/images/fox4.jpg',
     ])),
   );
 });
